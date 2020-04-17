67fb6e2db7a920165d79348282e521b8a61ea669
Aaron Son <aaron@liquidata.co>
2020-04-17 16:51:28 -0700

    docker/build.sh: Fixup build.sh to build ui as well.

---
+++ docker/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eo pipefail
+
+(cd ..; make bootstrap; make static-dist; XC_OSARCH=linux/amd64 make bin)
+cp ../bin/vault .
+docker build -t 200313983415.dkr.ecr.us-east-2.amazonaws.com/liquidata/vault:v1.3.2-aws-patch .
+docker push 200313983415.dkr.ecr.us-east-2.amazonaws.com/liquidata/vault:v1.3.2-aws-patch
